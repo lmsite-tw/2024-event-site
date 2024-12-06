aaf630e5b51afd80e9ba66306fb8bec6a3e30622
吳元皓 <hpware3@gmail.com>
2024-12-07 01:58:56 +0800

    Refactor UI in App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,13 @@ async function getProgram() {
 onMounted(getProgram);
 </script>
 <template>
-  <nav><h1 class="title">2024 聖誕音樂會 節目單</h1></nav>
+  <nav>
+    <div class="blur">
+      <h1 class="title">2024 聖誕音樂會 節目單</h1>
+    </div>
+  </nav>
     <div v-if="loading" >
-        <h2>載入中...</h2>
+        <h2>載入資料中...</h2>
       </div>
       <div v-if="!loading" class="content">
       <p v-for="(i, index) in pgr1" :key="index" class="maincontent">
@@ -32,28 +36,3 @@ onMounted(getProgram);
       </p>
       </div>
 </template>
-<style scoped>
-p.maincontent {
-  font-size: 16px;
-  text-align: center;
-}
-nav {
-  background-color:#2B2D30FF;
-  width:fill;
-  padding:10px;
-  top:0;
-  left:0;
-  right:0;
-  margin:0;
-  position:sticky;
-  z-index:100;
-  margin-bottom:0px;
-}
-h1.title {
-  width:fill;
-  text-align: center;
-  font-size: 30px;
-  margin-top:0;
-  margin-bottom:0;
-}
-</style>
