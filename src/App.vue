e0947a903bb6c68ec5eb4fdb5f5f9b3ba892f096
吳元皓 <hpware3@gmail.com>
2024-12-06 22:44:16 +0800

    Update page title and add event tracking

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-// Import things
 import {ref, onMounted} from 'vue';
-// Set vars
 const loading = ref(true);
-//const list = ref('');
-//const name = ref('');
-//const pl = ref('');
-const pgr1 = ref([]);
-
-// Pull data
+const pgr1 = ref<Programs[]>([]);
+interface Programs {
+  id: number;
+  type: string;
+  name: string;
+  program: string;
+}
 async function getProgram() {
-  const pullURL = await fetch('http://v4.yuanhau.com/api/db/ai-yue-wu-2024-sheng-dan-yin-yue-hui');
+  const pullURL = await fetch('https://v4.yuanhau.com/api/db/ai-yue-wu-2024-sheng-dan-yin-yue-hui');
   const pullData = await pullURL.json();
   pgr1.value = pullData;
   loading.value = false;
-
-  //for (const item of pullData) {
-  //  list.value = item.num;
-  //  name.value = item.program;
-  //  pl.value = item.name;
-  //  }
-
 }
-// Run on load
 onMounted(getProgram);
 </script>
-
 <template>
   <nav><h1 class="title">2024 聖誕音樂會 節目單</h1></nav>
     <div v-if="loading" >
@@ -33,13 +23,11 @@ onMounted(getProgram);
       </div>
       <div v-if="!loading" class="content">
       <p v-for="(i) in pgr1" :key="index" class="maincontent" v-bind="programs">
-        <span v-if="i.null">{{i.num}}. ERR: 讀取錯誤</span>
-        <span v-else><span>{{i.id}}</span>. <span>{{ i.type }}</span> <span>{{ i.name }}</span> <span>{{ i.program }}</span></span>
+        <span><span>{{i.id}}</span>. <span>{{ i.type }}</span> <span>{{ i.name }}</span> <span>{{ i.program }}</span></span>
         <br />
       </p>
       </div>
 </template>
-
 <style scoped>
 p.maincontent {
   font-size: 16px;
